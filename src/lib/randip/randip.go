e69e3f8db6c43f60615958a554d6d06bb1b45660
Rob Cameron <rwcameron@gmail.com>
2014-06-29 23:50:52 -0700

    added random ip mgr, added ipcheck to black list ip blocks and martians

--- src/lib/randip/randip.go
+++ src/lib/randip/randip.go
@@ -3,6 +3,7 @@ package randip
 import (
   "encoding/binary"
   "net"
+  "errors"
   "math"
 )
 
@@ -27,24 +28,24 @@ func (r *RandIPv4Mgr) GetCurrentIP() net.IP {
   return net.IP(ipBytes)
 }
 
-func (r *RandIPv4Mgr) GetNextIP() net.IP {
+func (r *RandIPv4Mgr) GetNextIP() (net.IP, error) {
   if r.ip == uint32(math.Pow(2,32) - 1) {
     //max IP reached
     ipBytes := make([]byte,4,4)
     binary.BigEndian.PutUint32(ipBytes,r.ip)
-    return net.IP(ipBytes)
+    return net.IP(ipBytes), errors.New("IP Space exhausted")
   } else {
     //increment and return next
     r.ip = r.ip + 1
     ipBytes := make([]byte,4,4)
     binary.BigEndian.PutUint32(ipBytes,r.ip)
-    return net.IP(ipBytes)
+    return net.IP(ipBytes), nil
   }
 }
 
-func (r *RandIPv4Mgr) GetNextIPStr() string {
-  nextIP := r.GetNextIP()
-  return nextIP.String()
+func (r *RandIPv4Mgr) GetNextIPStr() (string, error) {
+  nextIP, err := r.GetNextIP()
+  return nextIP.String(), err
   //get current ip
   //increment
   //return
---
+++ src/tlstest.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "crypto/tls"
+    "log"
+    //"encoding/json"
+    "lib/randip"
+    "net"
+    "time"
+    "fmt"
+    //"lib/ipcheck"
+)
+
+func main() {
+  ripmgr := randip.NewRandIPv4Mgr(true,1249767200)
+  for {
+    newIP, err := ripmgr.GetNextIP()
+    if err != nil {
+      log.Println("IP Addr Exhausted")
+      return
+    } else {
+      log.Println(newIP.String())
+      config := tls.Config{InsecureSkipVerify: true,ServerName:"google.com"}
+      var err error
+      var newConn *tls.Conn
+      newConn, err = tls.DialWithDialer(&net.Dialer{Timeout:2*time.Second},"tcp",newIP.String() + ":443",&config)
+      if err != nil {
+        log.Println(err)
+      } else {
+        conState := newConn.ConnectionState()
+        fmt.Println(newConn.RemoteAddr(),conState.PeerCertificates[0].NotBefore,conState.PeerCertificates[0].NotAfter,conState.PeerCertificates[0].SerialNumber)
+        //jsonCert,_ := json.MarshalIndent(conState.PeerCertificates[0],""," ")
+        //fmt.Println(string(jsonCert))
+        newConn.Close()
+      }
+    }
+  }
+}
